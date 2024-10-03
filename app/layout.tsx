9f8a4597993c763cd0acdb08acdd4f5d9488c16d
AlmamyHaidara <almamyh27@gmail.com>
2024-10-03 15:44:46 +0000

    theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,8 @@ export default function RootLayout({
     <html lang="en" className="h-full">
       <body
         className={cn(
-          `${geistSans.variable} ${geistMono.variable} antialiased h-full max-w-5xl m-auto px-4 `
+          // `${geistSans.variable} ${geistMono.variable} antialiased h-full max-w-5xl m-auto px-4 `
+          `${geistSans.variable} ${geistMono.variable} antialiased h-full max-w-full m-auto px-4 `
         )}
       >
         <ThemeProvider
@@ -43,20 +44,20 @@ export default function RootLayout({
             disableTransitionOnChange
           >
 
-        <div className="pt-5">
+        <div className="pt-5 ">
           <Header />
         </div>
-        <div className="gap-5">
-          <div className="circlePosition w-[260px] h-[400px]  max-md:h-[300px] bg-primary/35 rounded-b-3xl absolute z-1 top-[20%] right-[20%]  max-md:right-[40%]  translate-x-[50%] translate-y-[-50%] rotate-45 blur-[90px]"></div>
+        <div className="gap-5 z-0">
+          <div className="z-0 circlePosition w-[260px] h-[400px] max-md:h-[300px] bg-primary/35 rounded-b-3xl absolute z-1 top-[30%] right-[20%]  max-md:right-[40%]  translate-x-[50%] translate-y-[-50%] rotate-45 blur-[90px]"></div>
         </div>
-        <div className="">
+        <div className=" max-w-full flex flex-1 justify-center items-center max-md:px-[3.5%] md:px-[20%]">
 
         {children}
         </div>
         <div className="">
           <Spacing />
         </div>
-        <div className="pt-5">
+        <div className="items-center  md:px-[18.6%]">
           <Footer />
         </div>
         </ThemeProvider>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,82 +1,119 @@
-"use client"
-import { cn } from '@/lib/utils'
-import { AlignRight, X } from 'lucide-react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React, { useState } from 'react'
-import { ModeToggle } from './ModeToggle'
+"use client";
+import { cn } from "@/lib/utils";
+import { AlignRight, X } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { useState } from "react";
+import { ModeToggle } from "./ModeToggle";
 
-const navigation= [
+const navigation = [
   {
-    id:1,
-    path:'/',
-    title:"Acceuil"
+    id: 1,
+    path: "/",
+    title: "Acceuil",
   },
   {
-    id:2,
-    path:'/outil',
-    title:"Outil"
+    id: 2,
+    path: "/outil",
+    title: "Outil",
   },
   {
-    id:3,
-    path:'/projets',
-    title:"Mes projets"
+    id: 3,
+    path: "/projets",
+    title: "Mes projets",
   },
   {
-    id:4,
-    path:'/blog',
-    title:"Blog"
+    id: 4,
+    path: "/blog",
+    title: "Blog",
   },
-]
+];
 function Header() {
-
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   const currentPath = usePathname();
-  const isActivate = (path:string) => (currentPath === path ? "border-b-3 max-md:rounded-none shadow-sl shadow-primary/10 bg-primary/15 text-primary  rounded-md " : "")
-  const visibilityClass = isOpen ? 'max-md:!block' : '';
+  const isActivate = (path: string) =>
+    currentPath === path
+      ? "border-b-3 max-md:rounded-none shadow-sl shadow-primary/10 bg-primary/15 text-primary  rounded-md "
+      : "";
+  const visibilityClass = isOpen ? "max-md:!block" : "";
 
-  const isMenuOpen = ()=>{
-    
-    setIsOpen(prev=>!prev)
-    console.log("oi",isOpen);
-
-  }
+  const isMenuOpen = () => {
+    setIsOpen((prev) => !prev);
+    console.log("oi", isOpen);
+  };
   return (
-   <header className='flex items-center gap-2'>
-    <Link href="/" className='rounded-full flex md:w-[14%] max-md:w-full justify-between items-center'>
-    <Image src={"/assets/images/logo1.png"} alt="alt" width={50} height={20} className='rounded-full'/>
-    <p className='font-extrabold text-3xl sm:text-xl max-lg:hidden '>Code<span className=' text-primary'>Craft</span></p>
-    </Link>
+    <header className="flex items-center gap-2 px-16 ">
+      <Link
+        href="/"
+        className="rounded-full flex md:w-[4%] max-md:w-full justify-between items-center"
+      >
+        <Image
+          src={"/assets/images/logo1.png"}
+          alt="alt"
+          width={50}
+          height={20}
+          className="rounded-full"
+        />
+        <p className="font-extrabold text-3xl sm:text-xl max-lg:hidden ">
+          Code<span className=" text-primary">Craft</span>
+        </p>
+      </Link>
 
-    <div className="ml-auto "> </div>
-    <div className="  flex justify-between">
-<div className="absolute right-5 top-10 z-50 max-md:block md:hidden cursor-pointer" onClick={isMenuOpen}>
-  {isOpen ? (
-    <X size={32} className={cn(`transform transition-all duration-500 ease-in-out ${visibilityClass}`)} />
-  ) : (
-    <AlignRight size={32} className={cn(`transform transition-all duration-500 ease-in-out ${visibilityClass}`)} />
-  )}
-</div>
+      <div className="ml-auto "> </div>
+      <div className="  flex justify-between">
+        <div
+          className="absolute right-5 top-10 z-50 max-md:block md:hidden cursor-pointer"
+          onClick={isMenuOpen}
+        >
+          {isOpen ? (
+            <X
+              size={32}
+              className={cn(
+                `transform transition-all duration-500 ease-in-out ${visibilityClass}`
+              )}
+            />
+          ) : (
+            <AlignRight
+              size={32}
+              className={cn(
+                `transform transition-all duration-500 ease-in-out ${visibilityClass}`
+              )}
+            />
+          )}
+        </div>
 
-<div className={`flex justify-between gap-5  max-md:bg-[#11151d] max-md:absolute max-md:left-0 max-md:top-0 max-md:right-0 max-md:py-20 max-md:bg-muted max-md:z-40 max-md:flex-col transform transition-all duration-500 ease-in-out ${isOpen ? "max-md:max-w-full max-md:translate-x-0 max-md:opacity-100 max-md:visible" : "max-md:max-w-0 max-md:translate-x-full max-md:opacity-0 max-md:invisible"}`}>
-
-{
-  navigation.map((nav,index)=>(
-
-    <Link href={nav.path}  key={index} className={cn(`${'text-primary border-primary transition-colors duration-500 p-2 max-md:hover:bg-primary/20 max-md:hover:transition-colors max-md:hover:duration-500 flex max-md:justify-start max-md:pl-10 '}  ${isActivate(nav.path)}`)}  >{nav.title}</Link>
-  ))
-}
-<ModeToggle />
-
-</div>
-    {/* <Link href="/" className='text-primary'>Acceuil</Link>
+        <div
+          className={`flex justify-between gap-5  max-md:bg-[#11151d] max-md:absolute max-md:left-0 max-md:top-0 max-md:right-0 max-md:py-20 max-md:bg-muted max-md:z-40 max-md:flex-col transform transition-all duration-500 ease-in-out ${
+            isOpen
+              ? "max-md:max-w-full max-md:translate-x-0 max-md:opacity-100 max-md:visible"
+              : "max-md:max-w-0 max-md:translate-x-full max-md:opacity-0 max-md:invisible"
+          }`}
+        >
+          {navigation.map((nav, index) => (
+            <Link
+              href={nav.path}
+              key={index}
+              className={cn(
+                `${"text-primary border-primary transition-colors duration-500 p-2 max-md:hover:bg-primary/20 max-md:hover:transition-colors max-md:hover:duration-500 flex max-md:justify-start max-md:pl-10 "}  ${isActivate(
+                  nav.path
+                )}`
+              )}
+            >
+              {nav.title}
+            </Link>
+          ))}
+          <div className=" max-md:pl-10 max-md:w-full ">
+            <ModeToggle />
+          </div>
+        </div>
+        {/* <Link href="/" className='text-primary'>Acceuil</Link>
     <Link href="/blog"  className={cn(`${'text-primary border-b-2  border-blue-700'} ${({ isActive }:{isActive:boolean}) => (isActive ? "bg-red-500" : "")} `)}>Blog</Link>
     <Link href="#" className='text-primary' >Contact</Link> */}
-    </div>
-   </header>
-  )
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
