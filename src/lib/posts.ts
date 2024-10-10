3c5bba2ecdee7f2123a11be5bbdf315eaaf0f62c
AlmamyHaidara <almamyh27@gmail.com>
2024-10-10 13:27:15 +0000

    dd

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -4,10 +4,10 @@ import path from "path";
 import { z } from "zod";
 
 const PostSchema = z.object({
-  title: z.string().min(10),
+  title: z.string().min(10).max(65),
   description: z.string(),
   publishedAt: z.coerce.string(),
-  cover: z.string().optional().default(""),
+  cover:z.string().optional().default(""),
   published: z.boolean().optional().default(false),
   freelance: z.boolean().optional().default(false),
 });
@@ -17,51 +17,45 @@ export type Post = z.infer<typeof PostSchema> & {
   content: string;
 };
 
-const parsePostFile = async (fileName: string, postsDirectory: string): Promise<Post | null> => {
-  const fullPath = path.join(postsDirectory, fileName);
-  const fileContent = await fs.readFile(fullPath, "utf-8");
-  const frontmatter = matter(fileContent);
+export const getMakdowns = async (dirName:string) => {
+  console.log("---dirName",dirName);
   
-  const safeData = PostSchema.safeParse(frontmatter.data);
-  
-  if (!safeData.success) {
-    console.error(`Erreur lors du parsing du fichier: ${fileName}`);
-    safeData.error.issues.forEach((issue) => {
-      console.error(`  - ${issue.path.join(" -> ")}: ${issue.message}`);
-    });
-    return null; // Retourne null en cas d'erreur
-  }
-  
-  if (!safeData.data.published && process.env.NODE_ENV !== "development") {
-    return null; // Ignore les posts non publiés
-  }
-  
-  return {
-    ...safeData.data,
-    slug: fileName.replace(/^\d+-/, "").replace(".mdx", ""),
-    content: frontmatter.content,
-  };
-};
-
-export const getMakdowns = async (dirName:string ): Promise<Post[]> => {
-  
-  // const postsDirectory = path.join(process.cwd(), dirName);
-  const postsDirectory = path.join( process.cwd(),dirName);
-  
-  console.log("uytrtyu",path.join( process.cwd(),dirName));
-  console.log("uytrtyuoooooo",  await fs.readdir(process.cwd()));
-  await fs.access(postsDirectory);
-
+  const postsDirectory = path.join(process.cwd(), dirName);
   const files = await fs.readdir(postsDirectory);
   const fileNames = files.filter((f) => f.endsWith(".mdx"));
 
-  const posts = await Promise.all(fileNames.map(async (fileName) => await  parsePostFile(fileName, postsDirectory)));
+  const posts: Post[] = [];
+  for await (const fileName of fileNames) {
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContent = await fs.readFile(fullPath, "utf-8");
+    const frontmatter = matter(fileContent);
+    
+    const safeData = PostSchema.safeParse(frontmatter.data);
+    
+    if (!safeData.success) {
+      console.error(`Error parsing file: ${fileName}`);
+      safeData.error.issues.forEach((issue) => {
+        console.error(`  - ${issue.path.join(" -> ")}: ${issue.message}`);
+      });
+      continue;
+    }
+    
+    if (!safeData.data.published && process.env.NODE_ENV !== "development") {
+      continue;
+    }
+    
+    posts.push({
+      ...safeData.data,
+      slug: fileName.replace(/^\d+-/, "").replace(".mdx", ""),
+      content: frontmatter.content,
+    });
+  }
   
-  // Filtrer les posts null
-  return posts.filter((post): post is Post => post !== null);
+  return posts;
 };
 
-export const getMakdown = async (slug: string, dir: string): Promise<Post | undefined> => {
+export const getMakdown = async (slug: string, dir:string) => {
   const posts = await getMakdowns(dir);
+  console.log("yyyyy",posts);
   return posts.find((post) => post.slug === slug);
 };
