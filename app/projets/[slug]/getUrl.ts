ce81e4beca59b00379d6306fb5a7715d79b2460a
AlmamyHaidara <almamyh27@gmail.com>
2024-10-11 10:13:47 +0000

    Deployement

--- app/projets/[slug]/getUrl.ts
+++ app/projets/[slug]/getUrl.ts
@@ -1,6 +1,6 @@
 export const getUrl = () => {
   if (process.env.NODE_ENV === "production") {
-    return "https://codecraft.ml";
+    return "https://www.codecraft.ml";
   }
 
   return "http://localhost:3000";
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@ import { withContentlayer } from "next-contentlayer2";
 
 const nextConfig = {
   reactStrictMode: false,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    });
+    return config;
+  },
 
 };
 
