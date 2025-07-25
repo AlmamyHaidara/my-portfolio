fb4e6ed86dc8f9c342e3e02cc91d31f17d141f9f
Almamy Ali Haidara <almamyh27@gmail.com>
2025-07-25 01:26:46 +0000

    mm

--- src/features/mdx/Mdx.tsx
+++ src/features/mdx/Mdx.tsx
@@ -5,10 +5,7 @@ import { MDX_COMPONENTS } from "./mdx-components";
 export const Mdx = ({ children }: { children: string }) => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <MDXRemote
-        source={children}
-        components={MDX_COMPONENTS}
-      />
+      <MDXRemote source={children} components={MDX_COMPONENTS} />
     </Suspense>
   );
 };
